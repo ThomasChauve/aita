1cd758ec9f2c3bf27b965b261463c9c286b9a410
Chauve <thomas.chauve@lgge.obs.ujf-grenoble.fr>
2018-11-20 16:11:07 +0100

    Add files via upload

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,49 +1,46 @@
-.. G50 AITA toolbox documentation master file, created by
-   sphinx-quickstart on Fri Jul 17 18:14:29 2015.
+.. AITAToolbox documentation master file, created by
+   sphinx-quickstart on Tue Nov 20 10:23:06 2018.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to G50 AITA toolbox's documentation!
-============================================
+Welcome to AITA Toolbox's documentation!
+========================================
 
-Contents: this toolbox is build to analysed output from G50 analyser (aita)
+AITA Toolbox is a python tools created to work on orientation map such as the one obtained using optical measurement (birefringence). It is an open source code under CC-BY-CC licence (https://creativecommons.org/licenses/by/2.0/fr/deed.en). There is no guarantee if you are using it. It has been tested with python 3.7
+
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 1
+   :caption: Documentation:
+
+   installing
 
 
-Script exemples
-===============
+.. toctree::
+    :maxdepth: 1
+    :numbered:
+    :caption: AITA
 
-.. automodule:: exemple
-	:members:
+    AITA/firststep
+    AITA/function
 
-AITA Toolbox
-============
-**Toolbox module**
+.. toctree::
+    :maxdepth: 1
+    :numbered:
+    :caption: CLASS
 
-.. automodule:: aita
-	:special-members:
-	:members:
+    CLASS/image2d
+    CLASS/setvector3d
 
-**AITA open data**
- .. automodule:: loadData_aita
-	:special-members:
-	:members:  
 
-Other object defined
-====================
-.. automodule:: image2d
-   :special-members:
-   :members:
+Contact
+=======
+:Author: Thomas Chauve
+:Contact: thomas.chauve@univ-grenoble-alpes.fr
 
-.. automodule:: micro2d
-   :special-members:
-   :members:
-
-.. automodule:: mask2d
-   :special-members:
-   :members:
+:organization: UiO
+:status: This is a "work in progress"
+:version: 2.0
 
 
 
@@ -53,4 +50,3 @@ Indices and tables
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
-
---
+++ src/AITAToolbox/loadData_aita.py
@@ -0,0 +1,95 @@
+import AITAToolbox.aita as aita
+import numpy as np
+import math
+from skimage import io
+from PIL import Image
+
+def aita5col(data_adress,micro_adress=0):
+    '''
+    Function to open data from AITA analyser using 'cistodat' which give output file with 5 column (x,y,azi,col,qua)
+    
+    :param data_adress: orientation.dat file
+    :type data_adress: str
+    :param micro_adress: .bmp (24 bit) with black background and grains boundary in white
+    :type micro_adress: str
+    :return: aita object
+    :rtype: aita
+    '''
+    # load data from G50 output
+    file=open(data_adress,'r')
+    azi,col,qua = np.loadtxt(file, skiprows=19,usecols=(2,3,5),dtype='f,f,f',comments='[eof]',unpack=True)    
+    file.close()
+    # read head of file
+    file=open(data_adress,'r')
+    a=[]
+    [a.append(file.readline()) for i in list(range(16))]
+    file.close()
+    # resolution mu m
+    res=int(a[5][10:12])
+    # transforme the resolution in mm
+    resolution=res/1000. 
+    # number of pixel along x
+    nx=int(a[14][9:14])
+    # number of pixel along y
+    ny=int(a[15][9:13])
+    
+    # reashape the vector to a matrix
+    # use Bunge Euler angle convention
+    phi1_field=np.mod((azi.reshape((ny,nx))+90)*math.pi/180,2*math.pi)
+    phi_field=col.reshape((ny,nx))*math.pi/180
+    qua_field=qua.reshape((ny,nx))
+    
+    #open micro.bmp if necessary
+    if micro_adress==0:
+        micro_field=np.zeros((ny,nx))
+    else:
+        micro_bmp = io.imread(micro_adress)
+        mm=np.max(micro_bmp)
+        micro_field=micro_bmp[:,:,0]/mm
+        
+    return aita.aita(phi1_field,phi_field,qua_field,micro_field,resolution=resolution)
+
+def aita3col(data_adress, im_adress, micro_adress=0):
+    '''
+    Function to open data from AITA analyser using 'cistodat' which give output file with 3 column azi,col,qua)
+    Require to load an image file to get the image dimension nx, ny
+    
+    :param data_adress: orientation.dat file
+    :type data_adress: str
+    :param im_adress: any .bmp image file from AITA G50
+    :type im_adress: str
+    :param micro_adress: .bmp (24 bit) with black background and grains boundary in white
+    :type micro_adress: str
+    :return: aita object
+    :rtype: aita
+    '''
+    # load data from G50 output
+    with open(data_adress,'r') as file:    
+        azi, col, qua = np.loadtxt(file, skiprows=16,comments='[eof]',unpack=True)
+    
+    with open(data_adress,'r') as file:
+        a = [file.readline() for i in range(16)]
+    # resolution mu m
+    res=int(a[5][10:12])
+    # transforme the resolution in mm
+    resolution=res/1000. 
+
+    im = Image.open(im_adress)
+    [nx,ny] = im.size
+    
+    # reashape the vector to a matrix
+    # use Bunge Euler angle convention
+
+    phi1_field = np.mod((azi.reshape((ny,nx))+90)*math.pi/180,2*math.pi)
+    phi_field = col.reshape((ny,nx))*math.pi/180
+    qua_field = qua.reshape((ny,nx))
+    
+    #open micro.bmp if necessary
+    if micro_adress==0:
+        micro_field=np.zeros((ny,nx))
+    else:
+        micro_bmp = io.imread(micro_adress)
+        mm=np.max(micro_bmp)
+        micro_field=micro_bmp[:,:,0]/mm
+    
+    return aita.aita(phi1_field,phi_field,qua_field,micro_field,resolution=resolution)

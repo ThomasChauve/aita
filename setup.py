98e2b7ae45c0e3016d32a82e97d0a1021802a5f9
Thomas Chauve <thomaschauve@disroot.org>
2021-02-09 11:33:57 +0100

    update setup.py for package

--- setup.py
+++ setup.py
@@ -3,9 +3,40 @@ from setuptools import setup
 setup(name='AITAToolbox',
       version='2.0',
       description='Tools for AITA',
-      #url='http://github.com/storborg/funniest',
+      
+      url='https://github.com/ThomasChauve/aita',
       author='Thomas Chauve',
       author_email='thomas.chauve@univ-grenoble-alpes.fr',
-      license='CC-BY-CC',
+      license='GPL-3.0',
       packages=['AITAToolbox'],
-      zip_safe=False)
+      zip_safe=False
+     
+     classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',],
+      
+      project_urls={
+        'Source': 'https://github.com/ThomasChauve/aita',
+        'Tracker': 'https://github.com/ThomasChauve/aita/issues',
+        'Documentation': 'https://thomaschauve.github.io/aita/build/html/index.html',
+      },
+      
+      python_requires='>=3.9',
+    
+      install_requires=[
+          'Shapely',
+          'pygmsh',
+          'numpy',
+          'matplotlib',
+          'scikit-image'
+          'scikit-learn',
+          'tqdm',
+          'scipy',
+          'vtk',
+          'Pillow'
+          'gmsh'
+      ],
+     
+     )

1ed8e3cf3a3e9c31442f115bff1585df7ebbf1f0
Thomas Chauve <thomaschauve@disroot.org>
2021-02-10 11:45:16 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,49 +1,125 @@
-import setuptools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
 import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'AITAToolbox'
+DESCRIPTION = 'Tools for AITA.'
+URL = 'https://github.com/ThomasChauve/aita'
+EMAIL = 'thomas.chauve@univ-grenoble-alpes.fr'
+AUTHOR = 'Thomas Chauve'
+REQUIRES_PYTHON = '>=3.9'
+VERSION = '2.1'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'Shapely','pygmsh','numpy','matplotlib','scikit-image','scikit-learn','tqdm','scipy','vtk','Pillow','gmsh'
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
 
 here = os.path.abspath(os.path.dirname(__file__))
-long_description = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
 
-setuptools.setup(
-    name='AITAToolbox',
-    version='2.1',
-    description='Tools for AITA',
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
     long_description=long_description,
-      
-    url='https://github.com/ThomasChauve/aita',
-    author='Thomas Chauve',
-    author_email='thomas.chauve@univ-grenoble-alpes.fr',
-    license='GPL-3.0',
-    packages=setuptools.find_packages(exclude=['docs','Exemple']),
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
 
-    
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='GPL-3.0',
     classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only'
     ],
-      
-    project_urls={
-        'Source': 'https://github.com/ThomasChauve/aita',
-        'Tracker': 'https://github.com/ThomasChauve/aita/issues',
-        'Documentation': 'https://thomaschauve.github.io/aita/build/html/index.html',
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
     },
-      
-    python_requires='>=3.9',
-    
-    install_requires=[
-        'Shapely',
-        'pygmsh',
-        'numpy',
-        'matplotlib',
-        'scikit-image',
-        'scikit-learn',
-        'tqdm',
-        'scipy',
-        'vtk',
-        'Pillow',
-        'gmsh'
-    ],
-     
 )

44379762420f3b8c61a1e235e466bb58ab8df112
Thomas Chauve <thomaschauve@disroot.org>
2021-02-10 09:23:39 +0100

    corrected setup file

--- setup.py
+++ setup.py
@@ -1,42 +1,44 @@
-from setuptools import setup
+import setuptools
 
-setup(name='AITAToolbox',
-      version='2.0',
-      description='Tools for AITA',
+setuptools.setup(
+    name='AITAToolbox',
+    version='2.0',
+    description='Tools for AITA',
       
-      url='https://github.com/ThomasChauve/aita',
-      author='Thomas Chauve',
-      author_email='thomas.chauve@univ-grenoble-alpes.fr',
-      license='GPL-3.0',
-      packages=['AITAToolbox'],
-      zip_safe=False
-     
-     classifiers=[
+    url='https://github.com/ThomasChauve/aita',
+    author='Thomas Chauve',
+    author_email='thomas.chauve@univ-grenoble-alpes.fr',
+    license='GPL-3.0',
+    packages=setuptools.find_packages(exclude=['docs','Exemple']),
+
+    
+    classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',],
+        'Programming Language :: Python :: 3 :: Only'
+    ],
       
-      project_urls={
+    project_urls={
         'Source': 'https://github.com/ThomasChauve/aita',
         'Tracker': 'https://github.com/ThomasChauve/aita/issues',
         'Documentation': 'https://thomaschauve.github.io/aita/build/html/index.html',
-      },
+    },
       
-      python_requires='>=3.9',
+    python_requires='>=3.9',
     
-      install_requires=[
-          'Shapely',
-          'pygmsh',
-          'numpy',
-          'matplotlib',
-          'scikit-image'
-          'scikit-learn',
-          'tqdm',
-          'scipy',
-          'vtk',
-          'Pillow'
-          'gmsh'
-      ],
+    install_requires=[
+        'Shapely',
+        'pygmsh',
+        'numpy',
+        'matplotlib',
+        'scikit-image',
+        'scikit-learn',
+        'tqdm',
+        'scipy',
+        'vtk',
+        'Pillow',
+        'gmsh'
+    ],
      
-     )
+)
